7a3ec4a1aadca842721fdceb9a5196b4206110e6
lanangksma <kusumalanang1@gmail.com>
2024-10-20 12:16:19 +0700

    feat(app): add new routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import routes from './routes/index.js';
+import sampahRoutes from './routes/sampahRoutes.js';
+import penjemputanRoutes from './routes/penjemputanRoutes.js';
+import dropboxRoutes from './routes/dropboxRoutes.js';
 
 const app = express();
 
 app.use(express.json());
 
 // Use routes
-app.use('/api', routes);
+app.use('/api', routes, sampahRoutes, penjemputanRoutes, dropboxRoutes);
 
 export default app;
