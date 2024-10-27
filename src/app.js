fa18b405b49c6fc5f4f69195c27542487ca2f05a
Muhamad Jamaludin <muhhjam@gmail.com>
2024-10-27 17:01:51 +0700

    add cors

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors'; // Change here
 import routes from './routes/index.js';
-import authRoutes from './routes/authRoutes.js'
-import userRoutes from './routes/userRoutes.js'
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import sampahRoutes from './routes/sampahRoutes.js';
 import penjemputanRoutes from './routes/penjemputanRoutes.js';
 import dropboxRoutes from './routes/dropboxRoutes.js';
 
 const app = express();
 
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], // Allowed domains
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    credentials: true // If you want to send cookies or other credentials
+};
+
 app.use(express.json());
+app.use(cors(corsOptions));
 
 // Use routes
 app.use('/api', routes, sampahRoutes, penjemputanRoutes, dropboxRoutes, authRoutes, userRoutes);
