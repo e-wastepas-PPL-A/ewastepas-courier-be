7703878ec3f38cf1dca08b8c2f725b1ef6a2826b
lanangksma <kusumalanang1@gmail.com>
2024-11-06 19:01:57 +0700

    add getCalculatePickupTotals, and getPickupRequestById

--- src/controllers/pickupController.js
+++ src/controllers/pickupController.js
@@ -17,6 +17,102 @@ const getAllPickupRequest = async (req, res) => {
     }
 }
 
+// Temporary
+const getPickupRequestById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const pickUpId = parseInt(id, 10);
+
+        const pickup = await prisma.$queryRawUnsafe(`SELECT * FROM vw_pickup_waste WHERE pickup_id = ${pickUpId}`);
+
+        if (pickup.length === 0) {
+            return res.status(404).json({ message: 'No pickup found for the given ID.' });
+        }
+
+        res.status(200).json({data: pickup});
+    } catch (e) {
+        console.error(e)
+        return res.status(500).json({error: e});
+    }
+}
+
+const getCalculatePickupTotals = async (req, res) => {
+    const today = new Date();
+    const { id } = req.params;
+    const year = today.getFullYear();
+    const monthlyData = [];
+    console.log(today)
+    try {
+        const courierId = parseInt(id, 10);
+        const totalDelivered = await prisma.pickup_waste.count({
+            where: {
+                pickup_status: "accepted",
+                courier_id: courierId,
+                updated_at: { gte: today }
+            }
+        });
+
+        const totalOnDelivery = await prisma.pickup_waste.count({
+            where: {
+                pickup_status: "pending",
+                courier_id: courierId,
+                updated_at: { gte: today }
+            }
+        });
+
+        const totalCanceled = await prisma.pickup_waste.count({
+            where: {
+                pickup_status: "cancelled",
+                courier_id: courierId,
+                updated_at: { gte: today }
+            }
+        });
+
+        const totalPoints = await prisma.courier_points.findFirst({
+            where: {
+                courier_id: courierId,
+                updated_at: { gte: today }
+            },
+            select: {
+                total_points: true,
+            }
+        });
+        for (let month = 0; month < 12; month++) {
+            const startOfMonth = new Date(year, month, 1);
+            const endOfMonth = new Date(year, month + 1, 1);
+
+            const totalMonthDelivered = await prisma.pickup_waste.count({
+                where: {
+                    pickup_status: "accepted",
+                    courier_id: courierId,
+                    created_at: {
+                        gte: startOfMonth,
+                        lt: endOfMonth
+                    }
+                }
+            });
+
+            monthlyData.push({
+                month: month + 1,
+                totalDelivered: totalMonthDelivered,
+            });
+        }
+
+        res.json({
+            todayTotals: {
+                totalDelivered,
+                totalOnDelivery,
+                totalCanceled,
+                totalPoints: totalPoints ? totalPoints.total_points : 0
+            },
+            monthlyTotals: monthlyData
+        });
+    } catch (error) {
+        console.error("Error fetching calculate pickup:", error);
+        res.status(500).json({ error: "An error occurred while fetching calculate pickup" });
+    }
+}
+
 const acceptPickupRequest = async (req, res) => {
     const {id} = req.params;
     const pickupId = parseInt(id, 10)
@@ -55,5 +151,7 @@ const acceptPickupRequest = async (req, res) => {
 
 export {
     getAllPickupRequest,
-    acceptPickupRequest
+    acceptPickupRequest,
+    getPickupRequestById,
+    getCalculatePickupTotals
 }
--- src/routes/pickupRoutes.js
+++ src/routes/pickupRoutes.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import {
     acceptPickupRequest,
-    getAllPickupRequest
+    getAllPickupRequest,
+    getPickupRequestById,
+    getCalculatePickupTotals
 } from '../controllers/pickupController.js';
 
 const router = express.Router();
 
 router.get('/pickup', getAllPickupRequest);
+router.get('/pickup/totals/:id', getCalculatePickupTotals);
+router.get('/pickup/:id', getPickupRequestById);
 router.post('/pickup/:id', acceptPickupRequest);
-
 
 export default router;
