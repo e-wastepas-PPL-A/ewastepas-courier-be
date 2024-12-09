75720c7c28eaba643f4149cc2587133923f064cf
Muhammad Rizki Haikal <mrizhs294@gmail.com>
2024-12-09 23:53:38 +0700

    fix bugs

--- src/middleware/imageUploadMiddleware.js
+++ src/middleware/imageUploadMiddleware.js
@@ -1,9 +1,16 @@
 import multer from 'multer'
 import path from 'path'
+import fs from 'fs'
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'uploads/images')
+      const uploadPath = 'uploads'
+
+      if (!fs.existsSync(uploadPath)) {
+          fs.mkdirSync(uploadPath)
+      }
+
+      cb(null, uploadPath)
     },
     filename: function (req, file, cb) {
       const timestamp = Date.now()
