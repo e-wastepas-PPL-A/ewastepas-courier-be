636367e914c8e4a708cab2e363b47f841ea4814e
Muhammad Rizki Haikal <mrizhs294@gmail.com>
2024-12-09 11:06:16 +0700

    fix bug

--- src/controllers/userContoller.js
+++ src/controllers/userContoller.js
@@ -21,20 +21,24 @@ export const getUser = async (req, res) => {
 export const updateUserData = async (req, res) => {
     const {SERVER_URL} = process.env
     const id_payload = req.payload.id
-    const photoPath = req.files?.photo ? SERVER_URL+req.files.photo[0].path.replace(/\\/g, '/') : null;
-    const ktpPath = req.files?.ktp ? SERVER_URL+req.files.ktp[0].path.replace(/\\/g, '/') : null;
-    const kkPath = req.files?.kk ? SERVER_URL+req.files.kk[0].path.replace(/\\/g, '/') : null;
-    const {name, address, nik, date_of_birth, phone, account_number} = req.body
-    let data = {name, address, nik, date_of_birth, phone, account_number}
 
-    const files = {
-        ktp_url: ktpPath,
-        kk_url: kkPath,
-        photo:photoPath,
-    }
+    const filterRequest = Object.keys(req.body).reduce((result, key) => {
+        if (req.body[key]) { 
+          result[key] = req.body[key];
+        }
+        return result;
+      }, {});
 
-    if(files.ktp_url || files.kk_url || files.photo){
-        data = {...data, ...files}
+    if(req.files){
+        if (req.files.ktp){
+            filterRequest.ktp_url = SERVER_URL+req.files.ktp[0].path.replace(/\\/g, '/')
+        }
+        if (req.files.kk){
+            filterRequest.kk_url = SERVER_URL+req.files.kk[0].path.replace(/\\/g, '/')
+        }
+        if (req.files.photo){
+            filterRequest.photo = SERVER_URL+req.files.photo[0].path.replace(/\\/g, '/')
+        }
     }
 
     try {
@@ -42,8 +46,10 @@ export const updateUserData = async (req, res) => {
             where: {
                 courier_id: id_payload
             },
-            data: data
+            data: filterRequest
         })
+        console.log(filterFiles)
+        console.log(filterRequest)
         res.status(200).json({message: 'Data successfully updated', data: updatedCourier})
 
     } catch (error) {
