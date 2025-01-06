362f500860429693dcb0b703671d2afde0378f81
lanangksma <kusumalanang1@gmail.com>
2025-01-06 21:27:32 +0700

    feat: Add Google Cloud Logging integration to logger
    Enhanced the logger configuration by introducing `LoggingWinston` for Google Cloud Logging integration. Customized logging levels and added colorized console output for better readability. Created a new service account JSON file for authentication.

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,14 +1,41 @@
 import winston from 'winston';
+import { LoggingWinston } from "@google-cloud/logging-winston";
+
+const customLevels = {
+    levels: {
+        error: 0,
+        warn: 1,
+        info: 2,
+        debug: 3,
+    },
+    colors: {
+        error: 'red',
+        warn: 'yellow',
+        info: 'green',
+        debug: 'blue',
+    }
+};
+
+winston.addColors(customLevels.colors);
 
 export const logger = winston.createLogger({
-    level: 'info',
+    levels: customLevels.levels,
+    level: 'debug',
     format: winston.format.combine(
         winston.format.timestamp(),
-        winston.format.json() // This helps with structured logging
+        winston.format.json()
     ),
     transports: [
-        new winston.transports.Console(),
-        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'logs/combined.log' })
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({ filename: 'logs/app.log' }),
+        new LoggingWinston({
+            projectId: 'dragon-breath-423920',
+            keyFilename: 'dragon-breath-423920.json'
+        })
     ]
 });
